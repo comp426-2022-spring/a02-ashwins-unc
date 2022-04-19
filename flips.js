ea5f97be4d0d9ae1a99035275ee3e45f9939aacb
Ashwin <77787732+ashwins-unc@users.noreply.github.com>
2022-04-19 15:17:17 -0400

    Update flips.js

--- flips.js
+++ flips.js
@@ -2,4 +2,12 @@ import * as coin from './modules/coin.mjs';
 import minimist from "minimist";
 var arg = minimist(process.argv.slice(2));
 
-const num = arg["num
+const num = arg["number"] || 1;
+const array = coin.coinFlips(num);
+console.log(array)
+if(num == 1){
+    console.log({[array] : 1})
+}
+else{
+    console.log(coin.countFlips(array));
+}
