e99a62f3e7d541017167b67115391bf021e94449
Ashwin <77787732+ashwins-unc@users.noreply.github.com>
2022-04-19 15:18:59 -0400

    Update flips.js

--- flips.js
+++ flips.js
@@ -6,8 +6,5 @@ const num = arg["number"] || 1;
 const array = coin.coinFlips(num);
 console.log(array)
 if(num == 1){
-    console.log({[array] : 1})
-}
-else{
-    console.log(coin.countFlips(array));
+    console.log({[arra;
 }
