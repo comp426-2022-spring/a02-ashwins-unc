0d89fa4e8a6fe6a3b36404bf072056950055cca8
Ashwin <77787732+ashwins-unc@users.noreply.github.com>
2022-04-19 15:01:03 -0400

    Update flip.js

--- flip.js
+++ flip.js
@@ -1,3 +1,6 @@
 // Import the coinFlip function from your coin.mjs file
 
 // Call the coinFlip function and put the return into STDOUT
+import * as coin from './modules/coin.mjs';
+// Call the coinFlip function and put the return into STDOUT
+console.log(coin.coinFlip());
